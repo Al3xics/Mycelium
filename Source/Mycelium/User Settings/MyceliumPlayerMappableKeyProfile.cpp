3392e5886721ee71aa0d62616b33138d721de9f0
Alexis <alexis.martinho.78@gmail.com>
2026-01-13 13:58:26 +0100

    Add customizable input user settings and mappable key profile classes

---
+++ Source/Mycelium/User Settings/MyceliumPlayerMappableKeyProfile.cpp
@@ -0,0 +1,1 @@
+﻿#include "MyceliumPlayerMappableKeyProfile.h"
---
+++ Source/Mycelium/User Settings/MyceliumPlayerMappableKeyProfile.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "UserSettings/EnhancedInputUserSettings.h"
+#include "MyceliumPlayerMappableKeyProfile.generated.h"
+
+UCLASS()
+class MYCELIUM_API UMyceliumPlayerMappableKeyProfile : public UEnhancedPlayerMappableKeyProfile
+{
+	GENERATED_BODY()
+};
